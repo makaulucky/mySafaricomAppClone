ed1e790275ad767e07eac38e80107e1996ac8e19
makaulucky <makaulucky5@gmail.com>
2021-08-27 11:59:38 +0300

    Progress

--- components/TopBar.tsx
+++ components/TopBar.tsx
@@ -1,14 +1,21 @@
 import React, { Component } from 'react'
-import { Text, View,StyleSheet } from 'react-native'
+import { Text, View,StyleSheet,Button } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default class TopBar extends Component {
     render() {
         return (
             <SafeAreaView style={styles.container}>
-                <View style={styles.topbar}>
-                    <View  />
+                                
+                <View style={styles.topbar}>                    
                     <Text style={styles.title}> Good Morning Lucky </Text>
+                </View>
+                <View style={styles.button}>
+                    <Button
+                    styles={styles.button}
+                    title="View my balances"
+                    onPress={()=> alert('You pressed view my balance')}
+                    />
                 </View>
             </SafeAreaView>
         )
@@ -20,16 +27,22 @@ const styles= StyleSheet.create({
     container: {
         backgroundColor: "green",
         width:"100%",
-        height:70,
+        height:115,
         alignItems: "center",
         marginTop: 2,
 
     },
     title: {
-        fontSize: 20,
+        fontSize: 24,
+        padding: 15,
+        },
+    button: {
+        backgroundColor: "white",
+        fontSize: 24,
+
     },
 
     topbar: {
-        fontSize:20,
+        marginTop:3,
     },
 });
